64808962b52c6a8abf1c44475a967e68040ac47a
Darius3207 <115623014+Darius3207@users.noreply.github.com>
2023-09-13 09:18:09 +0300

    Create first-task.cpp

---
+++ PZ_2/first-task.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+int main()
+{
+  
+  int x;
+  int a;
+  int c;
+  double w;
+  double b;
+  
+  cout << "Enter x value: ";
+  cin >> x;
+  cout << "Enter A value: ";
+  cin >> a;
+  cout << "Enter constant value: ";
+  cin >> c;
+  
+  if (a == c) {
+      w = x * pow((a-c), 3);
+      b = x + (a, 3);
+      
+      cout << "The result is: " << w << endl;
+      cout << "The result is: " << b;
+      }
+      
+  else {
+      w = (x, 3) - a;
+      cout << "The result is: " << w;
+      }
+ 
+}
